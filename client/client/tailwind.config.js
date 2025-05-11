7c2b9459a27d6738478a9d412c85bd46386730e4
Ali Mukhtar <dr.kidri10@gmail.com>
2025-05-11 09:00:28 -0700

    Add Tailwind CSS config

---
+++ client/client/tailwind.config.js
@@ -0,0 +1,9 @@
+// Tailwind CSS configuration
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/*/.{js,jsx,ts,tsx}"], // Scan all source files
+  theme: {
+    extend: {}, // Extend Tailwind theme if needed
+  },
+  plugins: [], // Add Tailwind plugins if needed
+};
